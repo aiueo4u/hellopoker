b2bfd7b19f48ead60b0b4a23683b788e3e6ad4d9
aiueo4u <aiueo4u@gmail.com>
2017-10-01 21:00:17 +0900

    edit admin

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,5 +1,7 @@
 class Admin::HomeController < Admin::ApplicationController
   def index
+    @players = Player.all.to_a
+    @other_service_accounts = OtherServiceAccount.all.to_a
     @tables = Table.all.to_a
   end
 end
