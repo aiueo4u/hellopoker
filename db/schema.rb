fd378152e16dae47ab66b2b90d2f9fb0b8885641
aiueo4u <aiueo4u@gmail.com>
2017-10-03 03:06:41 +0900

    rename table index name

--- db/schema.rb
+++ db/schema.rb
@@ -21,27 +21,27 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["game_hand_id"], name: "index_game_actions_on_game_hand_id"
+    t.index ["game_hand_id"], name: "i1"
   end
 
   create_table "game_hand_players", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer "game_hand_id", unsigned: true
     t.integer "player_id", unsigned: true
-    t.index ["game_hand_id"], name: "index_game_hand_players_on_game_hand_id"
+    t.index ["game_hand_id"], name: "i1"
   end
 
   create_table "game_hands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer "table_id", unsigned: true
     t.integer "button_seat_no", unsigned: true
-    t.index ["table_id"], name: "index_game_hands_on_table_id"
+    t.index ["table_id"], name: "i1"
   end
 
   create_table "other_service_accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer "player_id", null: false, unsigned: true
     t.integer "provider", null: false, unsigned: true
     t.string "uid", null: false
-    t.index ["player_id", "provider"], name: "index_other_service_accounts_on_player_id_and_type", unique: true
-    t.index ["uid"], name: "index_other_service_accounts_on_uid", unique: true
+    t.index ["player_id", "provider"], name: "i1", unique: true
+    t.index ["uid"], name: "i2", unique: true
   end
 
   create_table "players", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
@@ -54,9 +54,9 @@ ActiveRecord::Schema.define(version: 0) do
     t.integer "player_id", null: false, unsigned: true
     t.integer "stack", default: 0, null: false, unsigned: true
     t.integer "seat_no", null: false, unsigned: true
-    t.index ["player_id"], name: "index_table_players_on_player_id"
-    t.index ["table_id", "player_id"], name: "index_table_players_on_table_id_and_player_id", unique: true
-    t.index ["table_id", "seat_no"], name: "index_table_players_on_table_id_and_seat_no", unique: true
+    t.index ["player_id"], name: "i1"
+    t.index ["table_id", "player_id"], name: "i2", unique: true
+    t.index ["table_id", "seat_no"], name: "i3", unique: true
   end
 
   create_table "tables", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
