37ae5b35eae5a8f2cbad08aadea99cc1a196554b
aiueo4u <aiueo4u@gmail.com>
2017-10-07 16:39:59 +0900

    fix table list

--- app/controllers/api/tables_controller.rb
+++ app/controllers/api/tables_controller.rb
@@ -2,7 +2,8 @@ class Api::TablesController < Api::ApplicationController
   before_action :check_jwt
 
   def index
-    @tables = Table.includes(:table_players).all
+    table_players = TablePlayer.where(player_id: current_player.id).all.to_a
+    @tables = Table.includes(:table_players).where(id: table_players.map(&:table_id)).to_a
     @player_by_id = Player.all.index_by(&:id)
   end
 
