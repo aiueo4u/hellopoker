8ba893cf56e15f29a1e2b97352bcac9c5216b0c3
aiueo4u <aiueo4u@gmail.com>
2018-08-22 01:30:19 +0900

    fix

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -8,7 +8,7 @@ class Api::ApplicationController < ActionController::API
   def set_current_client_version_header
     response.headers['Access-Control-Expose-Headers'] = 'Current-Client-Version'
     version = Rails.cache.fetch('current_client_version', expires_in: 1.minute) do
-      ClientVersion.order(created_at: :desc).first.version
+      ClientVersion.order(created_at: :desc).first&.version
     end
     response.headers['Current-Client-Version'] = version
   end
