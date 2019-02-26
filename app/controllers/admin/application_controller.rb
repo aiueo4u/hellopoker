e1a1f250bc6054c3b9342d0f9c2f822db6de5ef1
aiueo4u <aiueo4u@gmail.com>
2019-02-26 09:48:33 +0900

    [admin] BASIC認証

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,2 +1,13 @@
 class Admin::ApplicationController < ApplicationController
+  before_action :check_basic_authentication
+
+  private
+
+  def check_basic_authentication
+    name = 'poker'
+    passwd = '53ddd2b22a573040131269b9656ef013a37e3f66'
+    authenticate_or_request_with_http_basic('BA') do |n, p|
+      n == name && Digest::SHA1.hexdigest(p) == passwd
+    end
+  end
 end
