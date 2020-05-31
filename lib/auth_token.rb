38050c695a5a534f0c86cce630bffe50015c247d
Masahiro Yamada <aiueo4u@gmail.com>
2020-06-01 00:14:16 +0900

    fix jwt gem

--- lib/auth_token.rb
+++ lib/auth_token.rb
@@ -3,10 +3,10 @@ require 'jwt'
 class AuthToken
   def self.encode(payload)
     # TODO: 期限チェック
-    JWT.encode(payload, nil, false)
+    JWT.encode(payload, nil, 'none')
   end
 
   def self.decode(token)
-    JWT.decode(token, nil, false)[0]
+    JWT.decode(token, nil, 'none')[0]
   end
 end
