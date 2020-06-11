c3c7c2d3c01b443b5ce484d279f79fd2bb64e22c
Masahiro Yamada <aiueo4u@gmail.com>
2020-06-12 00:39:47 +0900

    fix

--- frontend/javascript/hooks/usePlayerActionTimer.js
+++ frontend/javascript/hooks/usePlayerActionTimer.js
@@ -3,14 +3,14 @@ import { useEffect, useState } from 'react';
 const usePlayerActionTimer = (player, gameTable) => {
   const [remainTime, setRemainTime] = useState(0);
 
-  const maxTimeToAction = (player && player.max_remain_time_to_action) || null;
-  const isPlayerTurn = player && player.seat_no === gameTable.currentSeatNo;
+  const maxTimeToAction = (player && player.maxRemainTimeToAction) || null;
+  const isPlayerTurn = player && player.seatNo === gameTable.currentSeatNo;
 
   useEffect(() => {
     if (!isPlayerTurn) return;
     if (!gameTable.inGame) return;
 
-    setRemainTime(player.remain_time_to_action);
+    setRemainTime(player.remainTimeToAction);
 
     const timer = setInterval(() => {
       setRemainTime(time => time - 1);
