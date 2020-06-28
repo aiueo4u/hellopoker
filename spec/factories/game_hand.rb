8b1b86984949ec45144f5d290aab58db0fcc3f2e
Masahiro Yamada <aiueo4u@gmail.com>
2020-06-29 03:39:13 +0900

    fix spec

--- spec/factories/game_hand.rb
+++ spec/factories/game_hand.rb
@@ -1,32 +1,26 @@
-FactoryBot.define do
-  factory :game_hand do
-
-    trait :init do
-      state  0
-    end
-
-    trait :preflop do
-      state 1
-    end
-
-    trait :flop do
-      state 2
-    end
-
-    trait :turn do
-      state 3
-    end
-
-    trait :river do
-      state 4
-    end
-
-    trait :result do
-      state 5
-    end
-
-    trait :finished do
-      state 6
-    end
-  end
-end
+#FactoryBot.define do
+# factory :game_hand do i# i#   trait :init do i#     state  0 i#   end i# i#   trait :preflop do
+#     state 1
+#   end
+#
+#   trait :flop do
+#     state 2
+#   end
+#
+#   trait :turn do
+#     state 3
+#   end
+#
+#   trait :river do
+#     state 4
+#   end
+#
+#   trait :result do
+#     state 5
+#   end
+#
+#   trait :finished do
+#     state 6
+#   end
+# end
+#end
