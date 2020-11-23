fb8d6295150c949cad6c18cf59da51c6988cff8a
Masahiro Yamada <aiueo4u@gmail.com>
2020-11-23 15:54:25 +0900

    remove unused

--- frontend/javascript/scenes/desktop/Home/Home.js
+++ frontend/javascript/scenes/desktop/Home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import { Box, Typography } from '@material-ui/core';
-import useStyles from './HomeStyles';
 
 const Home = () => {
-  const classes = useStyles();
-
   return (
     <Box p={2}>
-      <Typography variant="h5" align="center">Hello Poker</Typography>
+      <Typography variant="h5" align="center">
+        Hello Poker
+      </Typography>
     </Box>
   );
 };
