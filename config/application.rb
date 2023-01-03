66461cd382c4480cd058bd30c0a138b5c41fa315
Masahiro Yamada <masahiro.yamada@youtrust.jp>
2023-01-03 16:42:25 +0900

    Fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module HelloPoker
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version
-    config.load_defaults 6.0
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
