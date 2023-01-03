8f384d1b304308f16c97ff64e27e67aee388e84c
Masahiro Yamada <masahiro.yamada@youtrust.jp>
2023-01-03 15:34:54 +0900

    Add command.rb

---
+++ app/commands/concerns/command.rb
@@ -0,0 +1,28 @@
+module Command
+  extend ActiveSupport::Concern
+  include ActiveModel::Model
+
+  module ClassMethods
+    def run(**args)
+      new(**args).tap(&:run)
+    end
+  end
+
+  def run
+    raise NotImplementedError
+  end
+
+  def success?
+    errors.none?
+  end
+
+  private
+
+  def initialize
+    super
+  end
+
+  def raise_rollback
+    raise ActiveRecord::Rollback
+  end
+end
