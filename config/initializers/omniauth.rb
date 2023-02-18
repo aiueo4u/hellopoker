6f1f78b5ffbda7cd857bd8c23b1c496b79af8c1e
Masahiro Yamada <masahiro.yamada@youtrust.jp>
2023-02-19 03:00:32 +0900

    fix twitter login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Settings.twitter.consumer_key, Settings.twitter.consumer_secret, authorize_params: { force_login: true }
+  provider :twitter, Settings.twitter.consumer_key, Settings.twitter.consumer_secret, authorize_params: { force_login: true }, use_authorize: true
   provider :facebook, Settings.facebook.app_id, Settings.facebook.app_secret
 end
